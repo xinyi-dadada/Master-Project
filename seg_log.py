3abc087a9de791d41c8f98fc0361f41983755e06
xinyi-dadada <xinyi.cheng00@gmail.com>
2025-02-26 15:07:32 +0100

    Segmentation-Logistic Regression Function

--- seg_log.py
+++ seg_log.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import random
 
-
 class SegLogFunction():
     def __init__(self, base_dir):
         self.base_dir = base_dir
@@ -114,7 +113,7 @@ class SegLogFunction():
             if (input_index[0] <= truth_index[1] and input_index[1] >= truth_index[0]):
                 return label
 
-    def seg_part(self, data_part1, task_and_split, name=None):
+    def seg_part(self, data_part1, task_and_split):
         radarsignal = divide_data.process_each_participant(data_part1)
         radarsignal = radarsignal.astype(np.float64)
         A = 0.01
@@ -123,16 +122,13 @@ class SegLogFunction():
         f_radarsignal = self.log_func(radarsignal, A, B)
         f_radarsignal = 1 - np.squeeze(f_radarsignal)
 
-        radarsignal = np.squeeze(radarsignal)
-        x_axis_seconds = np.arange(len(radarsignal)) * 0.23 * (1 / 256)
-
         feature_index = np.where(f_radarsignal > 0.005)[0]
         buffer = 8000
         feature_split = {}
         index_num = feature_index[0]
         split_num = 1  # for name the split in feature_split
         for i in range(1, len(feature_index)):
-            if feature_index[i] - feature_index[i - 1] > 10000:
+            if feature_index[i] - feature_index[i - 1] > 100000:
                 # max() in case the index start from 0, and the end will not reach the end of the signal, so it's safe
                 feature_split[f'split_{split_num}'] = [max(index_num - buffer, 0), feature_index[i - 1] + buffer]
                 split_num += 1
@@ -147,135 +143,49 @@ class SegLogFunction():
                 data_part['label'] = None
                 data_part.at[0, 'label'] = label
                 all_split.append(data_part)
-        # plot: signal split by task id, result of logistic regression function, signal split by thresholding
-        task_index = divide_data.divide_task_index(data_part1)
-
-        plt.figure(figsize=(12, 16))
-        sns.set_theme(style='whitegrid')
-        sns.color_palette("pastel")
-        plt.subplots_adjust(hspace=0.5)
-        # plot the original signal and the true tasks
-        plt.subplot(3, 1, 1)
-        sns.lineplot(x=x_axis_seconds, y=radarsignal, color='grey', alpha=0.8)
-        for task_name in self.important_task:
-            start, end = task_index[task_name]
-            start *= 0.23 * (1 / 256)
-            end *= 0.23 * (1 / 256)
-            random_color = (random.random(), random.random(), random.random())
-            plt.axvspan(start, end, color=random_color, alpha=0.3, label=task_name)
-
-        plt.legend(loc='lower right')
-        plt.title('a', loc='left', fontsize=20)
-        plt.xlabel('sec', loc='right', fontsize=15)
-        # plot the resulf of logistic regression
-        plt.subplot(3, 1, 2)
-        sns.lineplot(x=x_axis_seconds, y=f_radarsignal, color='lightgreen', alpha=0.5)
-        plt.title('b', loc='left', fontsize=20)
-        plt.xlabel('sec', loc='right', fontsize=15)
-        # plot the segmented feature parts
-        plt.subplot(3, 1, 3)
-        sns.lineplot(x=x_axis_seconds, y=radarsignal, color='grey', alpha=0.8)
-        for key, value in enumerate(feature_split):
-            start, end = feature_split[value]
-            start *= 0.23 * (1 / 256)
-            end *= 0.23 * (1 / 256)
-            random_color = (random.random(), random.random(), random.random())
-            plt.axvspan(start, end, color=random_color, alpha=0.3, label=value)
-        plt.title('c', loc='left', fontsize=20)
-        plt.xlabel('sec', loc='right', fontsize=15)
-        plt.legend(loc='lower right')
-        plt.show()
-        plt.clf()
-        return all_split
-"""
-    def seg_part(self, data_part1, task_and_split, name=None):
-        radarsignal = divide_data.process_each_participant(data_part1)
-        radarsignal = radarsignal.astype(np.float64)
-        A = 0.01
-        q = 0.001
-        B = np.quantile(radarsignal, q=q)
-        f_radarsignal = self.log_func(radarsignal, A, B)
-        f_radarsignal = 1 - np.squeeze(f_radarsignal)
-
-        radarsignal = np.squeeze(radarsignal)
-        x_axis_seconds = np.arange(len(radarsignal)) * 0.23
-
-        feature_index = np.where(f_radarsignal > 0.005)[0]
-        buffer = 8000
-        feature_split = {}
-        index_num = feature_index[0]
-        split_num = 1  # for name the split in feature_split
-        for i in range(1, len(feature_index)):
-            if feature_index[i] - feature_index[i - 1] > 10000:
-                # max() in case the index start from 0, and the end will not reach the end of the signal, so it's safe
-                feature_split[f'split_{split_num}'] = [max(index_num - buffer, 0), feature_index[i - 1] + buffer]
-                split_num += 1
-                index_num = feature_index[i]
-
-        all_split = []
-        for key, value in enumerate(feature_split):
-            start, end = feature_split[value]
-            data_part = self.flatten_df(data_part1, start, end)
-            label = self.split_and_labels(task_and_split, (start, end))
-            if label is not None:
-                data_part['label'] = None
-                data_part.at[0, 'label'] = label
-                all_split.append(data_part)
-        
+        print('')
         # plot: signal split by task id, result of logistic regression function, signal split by thresholding
         task_index = divide_data.divide_task_index(data_part1)
         plt.figure(figsize=(12, 16))
         sns.set(style='whitegrid')
-        sns.color_palette("pastel")
-        plt.subplots_adjust(hspace=0.5)
+
         # plot the original signal and the true tasks
         plt.subplot(3, 1, 1)
-        sns.lineplot(x=x_axis_seconds, y=radarsignal, color='grey', alpha=0.8)
+        sns.lineplot(radarsignal, color='green', alpha=0.8, legend=False)
         for task_name in self.important_task:
-            try:
-                start, end = task_index[task_name]
-                start *= 0.23
-                end *= 0.23
-                random_color = (random.random(), random.random(), random.random())
-                plt.axvspan(start, end, color=random_color, alpha=0.3, label=task_name)
-            except:
-                continue
-
+            start, end = task_index[task_name]
+            random_color = (random.random(), random.random(), random.random())
+            plt.axvspan(start, end, color=random_color, alpha=0.3, label=task_name)
         plt.legend(loc='lower right')
-        plt.title('a', loc='left', fontsize=20)
-        plt.xlabel('sec', loc='right', fontsize=15)
+        plt.title('original and task')
         # plot the resulf of logistic regression
         plt.subplot(3, 1, 2)
-        sns.lineplot(x=x_axis_seconds, y=f_radarsignal, color='lightgreen', alpha=0.5)
-        plt.title('b', loc='left', fontsize=20)
-        plt.xlabel('sec', loc='right', fontsize=15)
+        sns.lineplot(x=np.arange(len(f_radarsignal)), y=f_radarsignal, color='green', alpha=0.5)
+        plt.title('logistic regression')
         # plot the segmented feature parts
         plt.subplot(3, 1, 3)
-        sns.lineplot(x=x_axis_seconds, y=radarsignal, color='grey', alpha=0.8)
+        sns.lineplot(radarsignal, color='green', alpha=0.8, legend=False)
         for key, value in enumerate(feature_split):
             start, end = feature_split[value]
-            start *= 0.23
-            end *= 0.23
             random_color = (random.random(), random.random(), random.random())
             plt.axvspan(start, end, color=random_color, alpha=0.3, label=value)
-        plt.title('c', loc='left', fontsize=20)
-        plt.xlabel('sec', loc='right', fontsize=15)
+        plt.title('split')
         plt.legend(loc='lower right')
-        plt.savefig(f'/home/Shared/xinyi/blob1/thesis/logs_seg/result{name}')
+        plt.show()
         plt.clf()
-"""
 
-
+        return all_split
 
 # Set the base path, subfolders, and task filename
-base_dir = '/home/Shared/xinyi/blob1/thesis/data/parquet_samples/'
+base_dir = ...
 seg_function = SegLogFunction(base_dir)
-radar_no = 112
+radar_no = ...
 file_pattern = os.path.join(base_dir, '**', f'radar_samples_192.168.67.{radar_no}*')
 # get all file names for this radar
 matching_files = glob.glob(file_pattern, recursive=True)
 all_split = []
-tasks = pd.read_csv('/home/Shared/xinyi/blob1/thesis/data/task.csv')
+tasks = pd.read_csv('~/thesis/data/task.csv')
+
 ### split by log_func ###
 for data_path in matching_files:
     divide_data = DivideData(data_path)
@@ -288,21 +198,6 @@ for data_path in matching_files:
         all_split += all_split_part
 
 df_all_split = pd.concat(all_split, ignore_index=True)
-save_path = '/home/Shared/xinyi/blob1/thesis/logs_seg/radar112_seg_all_2110.parquet'
+save_path = ...
 df_all_split.to_parquet(save_path)
 
-
-"""
-# plot for the result
-data_path = '/home/Shared/xinyi/blob1/thesis/data/parquet_samples/15_06_22/radar_samples_192.168.67.112_758.parquet'
-divide_data = DivideData(data_path)
-part, num = divide_data.divide_participants()
-name = 1
-for key, value in enumerate(part):
-    data_part = part[value]
-    data_part = data_part.reset_index(drop=True)
-    task_and_split = seg_function.get_task_index(data_part)
-    all_split_part = seg_function.seg_part(data_part, task_and_split, name)
-    name += 1
-
-"""
